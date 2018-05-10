fbcfd44e294eb9c959773dd97762af5e5f3603cf
Seb Gertz <sebgertz@gmail.com>
2018-05-10 16:11:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenue) {
   const selectedDrivers = [];
-  drivers.map(function(driver){ 
+  drivers.map(function(driver){
     if (parseInt(driver.revenue) > parseInt(revenue)){
       selectedDrivers.push(driver);
     }
   });
   return selectedDrivers;
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  
 }
